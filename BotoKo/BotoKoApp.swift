050e3a648afd3873936c4bfe593104c6c437d99d
Viennarz Curtiz <viennarzc@gmail.com>
2022-03-11 21:31:31 +0800

    Initial Commit

---
+++ BotoKo/BotoKoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotoKoApp.swift
+//  BotoKo
+//
+//  Created by Viennarz Curtiz on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BotoKoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
